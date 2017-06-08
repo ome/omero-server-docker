24a577eae0e18ff99e1ea475b9450970f38d2b04
Simon Li <spli@dundee.ac.uk>
2017-06-08 11:35:33 +0100

    Remove processing of /config/*.omero

--- 99-run.sh
+++ 99-run.sh
@@ -4,13 +4,5 @@ set -eu
 
 omero=/opt/omero/server/OMERO.server/bin/omero
 cd /opt/omero/server
-
-if stat -t /config/* > /dev/null 2>&1; then
-    for f in /config/*; do
-        echo "Loading $f"
-        $omero load "$f"
-    done
-fi
-
 echo "Starting OMERO.server"
 exec $omero admin start --foreground
