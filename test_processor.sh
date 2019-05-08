6be7c100864cc495a986be382d28a6f552b9f473
Simon Li <spli@dundee.ac.uk>
2019-05-08 12:43:51 +0100

    Work-around Java SSL bug
    See https://github.com/openmicroscopy/openmicroscopy/pull/5949

--- test_processor.sh
+++ test_processor.sh
@@ -14,8 +14,10 @@ SCRIPT=/omero/util_scripts/Dataset_To_Plate.py
 
 dataset_id=$(docker exec $PREFIX-server $OMERO obj -q -s localhost -u $OMERO_USER -w $OMERO_PASS new Dataset name=$DSNAME | cut -d: -f2)
 
+# Fixed in 5.5.0 https://github.com/openmicroscopy/openmicroscopy/pull/5949
+BUGFIX_ARGS="--skip upgrade"
 docker exec $PREFIX-server sh -c \
-    "touch /tmp/$FILENAME && $OMERO import -d $dataset_id /tmp/$FILENAME"
+    "touch /tmp/$FILENAME && $OMERO import $BUGFIX_ARGS -d $dataset_id /tmp/$FILENAME"
 
 docker exec $PREFIX-server $OMERO script launch $SCRIPT IDs=$dataset_id
 echo "Completed with code $?"
