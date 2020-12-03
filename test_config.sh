c978934295db063a21abef4a67e0d77b2193eaa6
Simon Li <spli@dundee.ac.uk>
2020-12-03 18:22:54 +0000

    Enable omero-certificates

--- test_config.sh
+++ test_config.sh
@@ -13,3 +13,7 @@ docker exec $PREFIX-server $OMERO config get --show-password
 
 [[ $(docker exec $PREFIX-server $OMERO config get custom.property.fromenv) = "fromenv" ]]
 [[ $(docker exec $PREFIX-server $OMERO config get custom.property.fromfile) = "fromfile" ]]
+
+# Check whether the certificates plugin worked, AES256-SHA is not enabled by
+# default so this command will fail if the certificates plugin failed
+docker exec test-server openssl s_client -cipher AES256-SHA -connect localhost:4064
